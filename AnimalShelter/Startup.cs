95d58b53889390ca8b1835e0fc8fc2686ee258e1
Jeff Chiu <jwchiu82@gmail.com>
2020-12-22 10:57:30 -0800

    add changes to views folders to match entity framework
    Co-authored-by: Joseph Nero <josephnero111@gmail.com>

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
 
-namespace TODOProjectName
+namespace AnimalShelter
 {
     public class Startup
     {
@@ -12,33 +14,37 @@ namespace TODOProjectName
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddEnvironmentVariables();
+                .AddJsonFile("appsettings.json");
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddEntityFrameworkMySql()
+                .AddDbContext<ToDoListContext>(options => options
+                .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             app.UseDeveloperExceptionPage();
-            
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
-
-            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Oopsie Doodle!");
+                await context.Response.WriteAsync("Something went wrong!");
             });
         }
     }
